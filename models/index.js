2e99309908dd7acb4337e485cb1d073bb1d4d8fd
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-24 20:23:17 +0700

    menambahkan index.js dan user.js

--- models/index.js
+++ models/index.js
@@ -6,8 +6,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const config = require(__dirname + '/../config/config.js')[env];
 
 let sequelize;
 if (config.use_env_variable) {
@@ -41,3 +41,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
